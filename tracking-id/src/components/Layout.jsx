03ca193c456b9d12efbfbc97c435d5abe62c51fd
Olamiwale <121311388+NosmanDent@users.noreply.github.com>
2025-01-29 10:55:34 +0000

    tracking

--- tracking-id/src/components/Layout.jsx
+++ tracking-id/src/components/Layout.jsx
@@ -14,11 +14,9 @@ export default function Layout() {
 
       <Footer /> */}
 
-      <div>
-        <h4 className="break-words whitespace-normal p-4">
-          arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#
-        </h4>
-      </div>
+      <h4 className="break-words whitespace-normal p-4">
+        arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#arhireuaegharhrgh%dfhdfjfdfjkfjfjkfdjfdhfjhfjfjh%fdhdfkjdfjfjfjh%jfgjkvjkhfjkfdjk$fgjfgjfgjkf%fjhfjhfgjhfgfhfdjdfmdfbjdfjdfvjdfvbdfvjfjfjd%fgbhhdghdfhfvbcdhfjhdfk%#fvhdfvjhdfvhdffdfdf#
+      </h4>
     </div>
   );
 }
